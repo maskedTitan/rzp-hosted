12c1cd95eef0c1bcc2f87bbcd0b64915938374c4
maskedTitan <harshamarri18@gmail.com>
2020-05-05 00:03:05 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CheckoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
